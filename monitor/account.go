2790cca628120f85949671ed26e94ef27219594f
Alex Harley <alexbruceharley@gmail.com>
2020-05-26 16:23:36 +0200

    feat: add logging for all accounts.sol events

---
+++ monitor/account.go
@@ -0,0 +1,74 @@
+package monitor
+
+import (
+	"context"
+
+	"github.com/celo-org/kliento/contracts"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
+)
+
+type accountProcessor struct {
+	ctx      context.Context
+	logger   log.Logger
+	address  common.Address
+	accounts *contracts.Accounts
+}
+
+func NewAccountsProcessor(ctx context.Context, logger log.Logger, address common.Address, accounts *contracts.Accounts) *accountProcessor {
+	return &accountProcessor{
+		ctx:      ctx,
+		logger:   logger,
+		address:  address,
+		accounts: accounts,
+	}
+}
+
+func (p accountProcessor) ObserveState() {
+	return
+}
+
+func (p accountProcessor) HandleLog(eventLog *types.Log) {
+	logger := p.logger.New("contract", "Accounts", "logIndex", eventLog.Index)
+	if eventLog.Address == p.address {
+		eventName, eventRaw, ok, err := p.accounts.TryParseLog(*eventLog)
+		if err != nil {
+			logger.Warn("Ignoring event: Error parsing accounts event", "err", err, "eventId", eventLog.Topics[0].Hex())
+			return
+		}
+		if !ok {
+			return
+		}
+
+		var params = make([]interface{}, 0)
+		params = append(params, "eventName", eventName)
+
+		switch e := eventRaw.(type) {
+		case *contracts.AccountsAttestationSignerAuthorized:
+			params = append(params, "account", e.Account, "signer", e.Signer)
+		case *contracts.AccountsVoteSignerAuthorized:
+			params = append(params, "account", e.Account, "signer", e.Signer)
+		case *contracts.AccountsValidatorSignerAuthorized:
+			params = append(params, "account", e.Account, "signer", e.Signer)
+		case *contracts.AccountsAttestationSignerRemoved:
+			params = append(params, "account", e.Account, "oldSigner", e.OldSigner)
+		case *contracts.AccountsVoteSignerRemoved:
+			params = append(params, "account", e.Account, "oldSigner", e.OldSigner)
+		case *contracts.AccountsValidatorSignerRemoved:
+			params = append(params, "account", e.Account, "oldSigner", e.OldSigner)
+		case *contracts.AccountsAccountDataEncryptionKeySet:
+			params = append(params, "account", e.Account, "dataEncryptionKey", e.DataEncryptionKey)
+		case *contracts.AccountsAccountNameSet:
+			params = append(params, "account", e.Account, "name", e.Name)
+		case *contracts.AccountsAccountMetadataURLSet:
+			params = append(params, "account", e.Account, "metadataUrl", e.MetadataURL)
+		case *contracts.AccountsAccountWalletAddressSet:
+			params = append(params, "account", e.Account, "walletAddress", e.WalletAddress)
+		case *contracts.AccountsAccountCreated:
+			params = append(params, "account", e.Account)
+		}
+
+		logEventLog(logger, params...)
+	}
+}
