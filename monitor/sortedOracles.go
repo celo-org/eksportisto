6e18c8e25c7952007defc12bde8c13203e10e8ef
diminator <dimi.dejonghe@gmail.com>
2020-05-27 13:14:32 +0200

    change metric name

--- monitor/sortedOracles.go
+++ monitor/sortedOracles.go
@@ -140,7 +140,7 @@ func (p sortedOraclesProcessor) ObserveMetric(opts *bind.CallOpts, stableTokenAd
 		return err
 	}
 
-	metrics.SortedOraclesRateMaxDeviation.Set(float64(blockTime - medianTimestamp.Uint64()))
+	metrics.SortedOraclesMedianTimestamp.Set(float64(blockTime - medianTimestamp.Uint64()))
 
 	return nil
 }
