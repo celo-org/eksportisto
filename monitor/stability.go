a59b2d9c61a3ef297935df32c32b25c24b4ecfbe
Nelson Taveras <ntaveras03@gmail.com>
2020-08-11 16:56:16 +0200

    move comment up

--- monitor/stability.go
+++ monitor/stability.go
@@ -133,8 +133,8 @@ func (p stabilityProcessor) HandleLog(eventLog *types.Log) {
 			event := eventRaw.(*contracts.ExchangeExchanged)
 			logEventLog(logger, "eventName", eventName, "exchanger", event.Exchanger, "soldGold", event.SoldGold, "sellAmount", event.SellAmount, "buyAmount", event.BuyAmount)
 
-			minSellAmountInWei := big.NewInt(1e6)
 			// Prevent updating the ExchangedRate metric for small trades that do not provide enough precision when calculating the effective price
+			minSellAmountInWei := big.NewInt(1e6)
 			if event.SellAmount.Cmp(minSellAmountInWei) < 0 {
 			    return
 			}
