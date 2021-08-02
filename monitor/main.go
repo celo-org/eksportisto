d77d16f8bd4b72edf8b51cb65499ddbf27d69256
bowd <bogdan@bowd.io>
2021-08-02 15:48:57 +0200

    Paralellise each tx parsing

--- monitor/main.go
+++ monitor/main.go
@@ -115,7 +115,7 @@ func Start(ctx context.Context, cfg *Config) error {
 	} else {
 		bigInt, ok := new(big.Int).SetString(cfg.FromBlock, 10)
 		if !ok {
-			return fmt.Errorf("Unable to parse FromBlock %s", cfg.FromBlock)
+			return fmt.Errorf("unable to parse FromBlock %s", cfg.FromBlock)
 		}
 		startBlock = bigInt
 	}
@@ -315,34 +315,41 @@ func blockProcessor(ctx context.Context, startBlock *big.Int, headers <-chan *ty
 			eventHandlers[exchangeRegistryID] = exchangeProcessor
 		}
 
-		for txIndex, tx := range txs {
-			txHash := tx.Hash()
+		for _txIndex, _tx := range txs {
+			txIndex := _txIndex
+			tx := _tx
 
-			txLogger := logger.New("txHash", txHash, "txIndex", txIndex)
+			g.Go(func() error {
+				txHash := tx.Hash()
+				txLogger := logger.New("txHash", txHash, "txIndex", txIndex)
 
-			receipt, err := cc.Eth.TransactionReceipt(transactionCtx, txHash)
-			if err != nil {
-				return err
-			}
+				receipt, err := cc.Eth.TransactionReceipt(transactionCtx, txHash)
+				if err != nil {
+					return err
+				}
 
-			logTransaction(txLogger, "gasPrice", tx.GasPrice(), "gasUsed", receipt.GasUsed)
+				logTransaction(txLogger, "gasPrice", tx.GasPrice(), "gasUsed", receipt.GasUsed)
 
-			metrics.GasPrice.Set(utils.ScaleFixed(tx.GasPrice()))
+				metrics.GasPrice.Set(utils.ScaleFixed(tx.GasPrice()))
 
-			for eventIdx, eventLog := range receipt.Logs {
-				parseAndLogEvent(txLogger, eventIdx, eventLog)
-			}
+				for eventIdx, eventLog := range receipt.Logs {
+					parseAndLogEvent(txLogger, eventIdx, eventLog)
+				}
+				return nil
+			})
 		}
 
 		if tipMode {
 			g.Go(func() error { return epochRewardsProcessor.ObserveMetric(opts) })
 			g.Go(func() error { return sortedOraclesProcessor.ObserveMetric(opts, h.Time) })
 			// Celo token processors
-			for _, processor := range celoTokenProcessors {
+			for _, _processor := range celoTokenProcessors {
+				processor := _processor
 				g.Go(func() error { return processor.ObserveMetric(opts) })
 			}
 			// processors
-			for _, processor := range exchangeProcessors {
+			for _, _processor := range exchangeProcessors {
+				processor := _processor
 				g.Go(func() error { return processor.ObserveMetric(opts) })
 			}
 		}
@@ -351,7 +358,8 @@ func blockProcessor(ctx context.Context, startBlock *big.Int, headers <-chan *ty
 			g.Go(func() error { return reserveProcessor.ObserveState(opts) })
 			g.Go(func() error { return sortedOraclesProcessor.ObserveState(opts) })
 			// Celo token processors
-			for _, processor := range celoTokenProcessors {
+			for _, _processor := range celoTokenProcessors {
+				processor := _processor
 				g.Go(func() error { return processor.ObserveState(opts) })
 			}
 		}
@@ -362,7 +370,8 @@ func blockProcessor(ctx context.Context, startBlock *big.Int, headers <-chan *ty
 			g.Go(func() error { return lockedGoldProcessor.ObserveState(opts) })
 			g.Go(func() error { return reserveProcessor.ObserveState(opts) })
 			// processors
-			for _, processor := range exchangeProcessors {
+			for _, _processor := range exchangeProcessors {
+				processor := _processor
 				g.Go(func() error { return processor.ObserveState(opts) })
 			}
 
