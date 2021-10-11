09a4fbbc48d380e506e8d43af4381fc848b2285c
Trevor Porter <trkporter@ucdavis.edu>
2021-10-11 14:07:45 +0100

    Catch errors from EnableCaching

--- indexer/handler.go
+++ indexer/handler.go
@@ -54,7 +54,10 @@ func (w *worker) newBlockHandler(block uint64) (*blockHandler, error) {
 // run starts the processing of a block by firing all processors
 // and a routine to collect rows from a channel
 func (handler *blockHandler) run(ctx context.Context) (err error) {
-	handler.registry.EnableCaching(ctx, handler.blockNumber)
+	err = handler.registry.EnableCaching(ctx, handler.blockNumber)
+	if err != nil {
+		return err
+	}
 	group, ctx := errgroup.WithContext(ctx)
 	rowsChan := make(chan *Row, 1000)
 
