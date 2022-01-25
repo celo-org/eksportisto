f2ee0b599f7749360e8de15fdccb548fd0d2a5af
Piers Powlesland <pierspowlesland@gmail.com>
2022-01-25 12:28:53 +0000

    Add debug_TraceTransaction timeout option
    This option allows the timeout provided to blockchain nodes when making
    debug_TraceTransaction calls to be configured.
    
    This update uses a new version of kliento that accepts the timeout
    parameter.
    
    Previously the timeout was hardcoded to 50s in kliento, but 50s was not
    sufficient for larger transactions.
    
    Going forward, to be safe I suggest 120s for the timeout.

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -34,6 +35,7 @@ func init() {
 	rootCmd.PersistentFlags().Int("monitoring-port", 8080, "Port for the prometheus server")
 	rootCmd.PersistentFlags().String("celo-node-uri", "", "URI for the Celo Blockchain Node")
 	rootCmd.PersistentFlags().Bool("profiling", false, "Enable pprof on the http server")
+	rootCmd.PersistentFlags().Duration("traceTransactionTimeout", time.Second*50, "The timeout to pass to the blockchain node when tracing, prviously hardocded to 50s so defaults to 50s, 120s is a recommended value to be able to trace all transactions as of Jan 2022")
 
 	rootCmd.AddCommand(publisherCmd)
 	rootCmd.AddCommand(indexerCmd)
@@ -44,10 +46,12 @@ func initConfig() {
 	viper.SetDefault("monitoring.port", 8080)
 	viper.SetDefault("monitoring.address", "127.0.0.1")
 	viper.SetDefault("monitoring.requestTimeoutSeconds", 24)
+	viper.SetDefault("traceTransactionTimeout", time.Second*50)
 	viper.BindPFlag("monitoring.port", rootCmd.Flags().Lookup("monitoring-port"))
 	viper.BindPFlag("indexer.mode", indexerCmd.Flags().Lookup("indexer-mode"))
 	viper.BindPFlag("celoNodeURI", rootCmd.Flags().Lookup("celo-node-uri"))
 	viper.BindPFlag("profiling", rootCmd.Flags().Lookup("profiling"))
+	viper.BindPFlag("traceTransactionTimeout", rootCmd.Flags().Lookup("traceTransactionTimeout"))
 
 	if cfgFile != "" {
 		// Use config file from the flag.
